0ebb245e436cd8a79449e9a16fafd3f4c2fd6b8b
Kyle Ruff <kyleruff@gmail.com>
2019-08-26 21:05:22 -0600

    pulling users now, connected to db

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,8 +2,8 @@ var db = require("../models");
 
 module.exports = function(app) {
   // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
+  app.get("/api/users", function(req, res) {
+    db.user.findAll({}).then(function(dbExamples) {
       res.json(dbExamples);
     });
   });
