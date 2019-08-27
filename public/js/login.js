d998f360a30852f6e647b968838dfd435fff3130
Kyle Ruff <kyleruff@gmail.com>
2019-08-26 20:06:59 -0600

    created login.js

---
+++ public/js/login.js
@@ -0,0 +1,26 @@
+$("#add-btn").on("click", function (event) {
+    event.preventDefault();
+
+    // make a newUser obj
+    var newUser = {
+        // username from username input
+        userusername: $("#username").val().trim(),
+        // password from password input
+        password: $("#password").val().trim(),
+
+    };
+
+    // send an AJAX POST-request with jQuery
+    $.post("/api/new", newUser)
+        // on success, run this callback
+        .then(function (data) {
+            // log the data we found
+            console.log(data);
+            // tell the user we're adding a user with an alert window
+            alert("Adding user...");
+        });
+
+    // empty each input box by replacing the value with an empty string
+    $("#username").val("");
+    $("#password").val("");
+
