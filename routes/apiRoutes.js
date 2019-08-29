f21b34bbfb6b549ebc2b6c6209afe18dc9b8047b
baysik <iambaysik@gmail.com>
2019-08-28 19:25:08 -0600

    fixed routing errors

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -9,15 +9,24 @@ module.exports = function(app) {
   });
 
   // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
-    });
+  app.get("/api/users/:id", function(req, res) {
+    // Find one user with the id in req.params.id and return them to the user with res.json
+    db.user
+      .findOne({
+        where: {
+          id: req.params.id
+        }
+      })
+      .then(function(dbuser) {
+        res.json(dbuser);
+      });
   });
 
   // Delete an example by id
   app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
+    db.Example.destroy({ where: { id: req.params.id } }).then(function(
+      dbExample
+    ) {
       res.json(dbExample);
     });
   });
